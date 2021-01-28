a93f46618bf2b8c876d0d7f255ddcfb7c6473757
Sean Granier <seanlg624@gmail.com>
2021-01-28 11:45:18 -0600

    Section 17 completed

--- section17.js
+++ section17.js
@@ -1,1 +1,68 @@
-let section = "17";
+// Lotto Number Exercise 
+
+// let lottoNumbers = [24, 60, 55, 1, 77, 98];
+
+// console.log(lottoNumbers)
+
+//Array Access Exercise
+
+// const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// // YOUR CODE BELOW HERE:
+
+// leaderboard[1] = 'Luna';
+
+// leaderboard[3] = 'Draco';
+
+
+// console.log(leaderboard);
+
+// let movieLine = ['Sean', 'Adrian'];
+
+// movieLine.push('Alicia');
+
+// movieLine.push('Amber');
+
+// movieLine.shift()
+
+
+
+// console.log(movieLine);
+
+// const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// // YOUR CODE GOES BELOW THIS LINE:
+// planets.shift();
+
+// planets.push('Saturn');
+
+// planets.unshift('Mercury');
+
+
+
+// console.log(planets);
+
+// let cats = ['fitz', 'Rascal'];
+
+// let dogs = ['tazz', 'Maddie', 'Dawson'];
+
+// let pets = cats.concat(dogs);
+
+// console.log(pets);
+
+//Nested Arrays Exercise
+
+// DO NOT TOUCH!!! (please)
+// const airplaneSeats = [
+//     ['Ruth', 'Anthony', 'Stevie'],
+//     ['Amelia', 'Pedro', 'Maya'],
+//     ['Xavier', 'Ananya', 'Luis'],
+//     ['Luke', null, 'Deniz'],
+//     ['Rin', 'Sakura', 'Francisco']
+// ];
+// // YOUR CODE GOES BELOW THIS LINE:
+// airplaneSeats[3][1]='hugo';
+
+
+
+// console.log(airplaneSeats);

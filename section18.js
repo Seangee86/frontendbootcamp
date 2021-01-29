83b72860848916a679208135b89ba9dcc0bcd5ee
Sean Granier <seanlg624@gmail.com>
2021-01-28 19:42:50 -0600

    sectiom 18 - practice

--- section18.js
+++ section18.js
@@ -1,2 +1,20 @@
 // SECTION 18
+// Intro Object Literals 
 
+// let product = {
+//     name : 'Gummy Bears',
+//     inStock : true,
+//     price : 1.99,
+//     flavors : ["grape", "apple", "cherry"]
+// }
+
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:

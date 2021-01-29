3f05129dabda32deb6594d8651b572a6ba739d20
Sean Granier <seanlg624@gmail.com>
2021-01-29 09:22:34 -0600

    section 18 complete

--- section18.js
+++ section18.js
@@ -9,12 +9,27 @@
 // }
 
 //PLEASE DON'T TOUCH THIS LINE!
-const restaurant = {
-    name: 'Ichiran Ramen',
-    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-    city: 'Brooklyn',
-    state: 'NY',
-    zipcode: '11206',
-}
+// const restaurant = {
+//     name: 'Ichiran Ramen',
+//     address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+//     city: 'Brooklyn',
+//     state: 'NY',
+//     zipcode: '11206',
+// }
 
-//YOUR CODE GOES DOWN HERE:
+// //YOUR CODE GOES DOWN HERE:
+// let fullAddress = restaurant.name + " " + restaurant.address + " " + restaurant.city + " " + restaurant.state + " " + restaurant.zipcode;
+
+// console.log(fullAddress);
+
+// let grades = {
+//     tim: 78,
+//     henry: 98
+// }
+// grades.henry = 100;
+
+// grades['tim'] = 88;
+
+// grades['paul'] = 76;
+
+// console.log(grades);

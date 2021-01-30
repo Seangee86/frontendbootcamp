99d63db62d92155726954fd49a2dc5a160345f69
Sean Granier <seanlg624@gmail.com>
2021-01-30 14:30:38 -0600

    section 19 complete

--- section19.js
+++ section19.js
@@ -89,19 +89,50 @@
 //     console.log(titles);
 // }
 
-const seatingChart = [
-    ['person1', 'person2', 'person3', 'person4'],
-    ['person5', 'person6', 'person7', 'person8'],
-    ['person9', 'person10', 'person11', 'person12'],
-    ['person13', 'person14', 'person15', 'person16']
-]
+// const seatingChart = [
+//     ['person1', 'person2', 'person3', 'person4'],
+//     ['person5', 'person6', 'person7', 'person8'],
+//     ['person9', 'person10', 'person11', 'person12'],
+//     ['person13', 'person14', 'person15', 'person16']
+// ]
 
-for (let row of seatingChart) {
-    console.log(row);
-}
+// for (let row of seatingChart) {
+//     console.log(row);
+// }
 
-for (let row of seatingChart) {
-    for (let student of row) {
-        console.log(student);
+// for (let row of seatingChart) {
+//     for (let student of row) 
+//         console.log(student);
+//     }
+// } 
+
+// const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (num of numbers) {
+//     console.log(num * num);
+// }
+
+// TODO List 
+
+let input = prompt("What would you like to do?")
+const todo = ['make bed', 'trash'];
+while (input !== 'quit' && input !== 'q') {
+    if(input === 'list') {
+        console.log('***********');
+        for (let i = 0; i < todo.length; i++) {
+            console.log(`${i}: ${todo[i]}`);
+        }
+        console.log('***********');
+    }else if (input === 'new') {
+        const newTodo = prompt('What is the new todo?');
+        todo.push(newTodo);
+        console.log(`${newTodo} added to the new list!`)
+    }else if (input === 'del') {
+        const index = prompt('index to del')
+        todo.slice(index, 1);
+        console.log(index);
     }
-} 
+    input = prompt("What would you like to do?")
+}
+console.log('ok, quitting')

82ca36c0c89c6cbddb90c13623ea22979ac4dc2a
Sean Granier <seanlg624@gmail.com>
2021-01-31 09:07:01 -0600

    section 20 - 9/15

--- section20.js
+++ section20.js
@@ -10,23 +10,48 @@
 // greet('Sean');
 // greet('Alicia');
 
-function rant(message) {
-    console.log(`${message}`.toUpperCase());
-    console.log(`${message}`.toUpperCase());
-    console.log(`${message}`.toUpperCase());
-}
-rant("I hate beets");
+// function rant(message) {
+//     console.log(`${message}`.toUpperCase());
+//     console.log(`${message}`.toUpperCase());
+//     console.log(`${message}`.toUpperCase());
+// }
+// rant("I hate beets");
 
-function rant(message) {
-    console.log(message.toUpperCase())
-    console.log(message.toUpperCase())
-    console.log(message.toUpperCase())
-}
-rant('I hate bees');
+// function rant(message) {
+//     console.log(message.toUpperCase())
+//     console.log(message.toUpperCase())
+//     console.log(message.toUpperCase())
+// }
+// rant('I hate bees');
 
-function rant(message) {
-    for(let i=1; i <= 3; i++) {
-        console.log(`${message}`.toUpperCase());
-    }
-}
-rant('I hate bees');
+// function rant(message) {
+//     for(let i=1; i <= 3; i++) {
+//         console.log(`${message}`.toUpperCase());
+//     }
+// }
+// rant('I hate bees');
+
+// function greet(firstName, lastName) {
+//     console.log(`Hey there ${firstName} ${lastName}`)
+// }
+// greet('Sean', 'gee');
+
+// function repeat(str, reps) {
+//     let result = "";
+//     for(let i = 0; i < reps; i++) {
+//        result += str;
+//     }
+//     console.log(result);
+// }
+// repeat('shit ', 10);
+
+// define isSnakeEyes below:
+
+// function isSnakeEyes(num1, num2) {
+//     if(num1 === 1 && num2 === 1) {
+//         console.log('snake eyes')
+//     }else {
+//         console.log('Not snake eyes')
+//     }
+// }
+// isSnakeEyes(1, 1);

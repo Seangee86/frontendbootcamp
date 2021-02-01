a78adfe04f3c75baa491427fc4bad7a3d854358c
Sean Granier <seanlg624@gmail.com>
2021-02-01 16:47:55 -0600

    Methods exercise

--- section20.js
+++ section20.js
@@ -92,3 +92,49 @@
 //     return firstLetter + restOfStr; 
 // }
 // capitalize("sean");
+
+//SUM ARRAY EXERCISE
+// function sumArray(arrNum) {
+//    let total = 0;
+//    for(let i = 0; i < arrNum.length; i++) {
+//        total += arrNum[i]
+//    }
+//    return total;
+// }
+// sumArray([1,2,3,4,5,4,3,4,5,4,5,6,5,4]);
+
+// DAYS OF THE WEEK EXERCISE
+
+// function returnDay(num) {
+//     let weeks = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+//     if (num < 1 || num > 7){
+//         return null;
+//     } else if (num === 1) {
+//         return weeks[0];
+//     }else if (num === 2) {
+//         return weeks[1];
+//     }else if (num === 3) {
+//         return weeks[2];
+//     }else if (num === 4) {
+//         return weeks[3];
+//     }else if (num === 5) {
+//         return weeks[4];
+//     }else if (num === 6) {
+//         return weeks[5];
+//     }else if (num === 7) {
+//         return weeks[6];
+//     }                    
+// return weeks;
+// }
+// returnDay(1);
+
+
+
+// function returnDay(num){
+//     if (num < 1 || num > 7){
+//         return null
+//     }    
+//     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+//     return days[num]
+// }
+// returnDay(1);
---
+++ section21.js
@@ -0,0 +1,14 @@
+// SCOPE
+
+//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  
+//You'll need to multiply a number by itself or use the Math.pow() method.
+
+// let square = function(num) {
+//     return num * num;
+// }
+// square(4);
+
+// const square = function(num) {
+//     return num * num;
+// }
+// square(4);

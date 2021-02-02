418b4b8e7842bca7cfa7ce2a82a4aabcf296a539
Sean Granier <seanlg624@gmail.com>
2021-02-02 15:18:50 -0600

    section 21 complete-start section 22

--- section21.js
+++ section21.js
@@ -12,3 +12,41 @@
 //     return num * num;
 // }
 // square(4);
+
+// const square = {
+//     area : function(a){
+//         return a * a;
+//     },
+//     perimeter : function(p) {
+//         return p * 4;
+//     }
+// }
+// square.area(10);
+
+
+// const square = {
+//     area: function(x){
+//       return x*x;
+//     },
+//     perimeter: function (x) {
+//       return 4*x;
+//     }
+//   }
+//   square.area(10);
+
+// const hen = {
+//     name: 'Helen',
+//     eggCount: 0,
+//     layAnEgg() {
+//         this.eggCount++;
+//         return "EGG";
+//     }
+// };
+// console.log(hen.layAnEgg());
+// console.log(hen.eggCount);
+// console.log(hen.layAnEgg());
+// console.log(hen.layAnEgg());
+// console.log(hen.layAnEgg());
+// console.log(hen.layAnEgg());
+// console.log(hen.layAnEgg());
+// console.log(hen.eggCount);
---
+++ section22.js
@@ -0,0 +1,51 @@
+// MAP
+// function cleanNames (names) {
+//     let newArr = names.map(function(trimmedArr) {
+//         return trimmedArr.trim();
+//     });
+//         return newArr;
+// }
+
+// ARROW FUNCTIONS
+
+// const greet = (name) => {
+//     return `Hey ${name}`
+// }
+// console.log(greet('sean'));
+// console.log(greet('Bob'));
+
+// const greet = (name) => {
+//     return name;
+// }
+// console.log(greet('sean'));
+// console.log(greet('Bob'));
+
+// FILTERS
+
+// function validUserName(name){
+//     return name.filter(char => char.length < 10);
+// }
+
+// validUserName(['sean','qwweerrfeffgsfsf', 'bob', 'paul', 'hdhdhdhdhdhdhdhdhdhdhdd']);
+
+
+
+// function validUserNames(userNames){
+//     return userNames.filter(characters => characters.length < 10);
+// }
+
+// function allEven(arr) {
+//     return arr.every(function(num) {
+//         return num % 2 === 0;
+//     });
+// }
+// allEven([2,4,6,8]);
+
+
+// // normal approach
+// function allEvens(arr) {
+//     return arr.every(function(num) {
+//         return num % 2 === 0;
+//     });
+// }
+ 

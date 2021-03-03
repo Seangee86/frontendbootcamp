46bd8c0d51478851d0438d9bd6de626909430c3d
Sean Granier <seanlg624@gmail.com>
2021-03-03 16:02:16 -0600

    update

--- Practice/app.js
+++ Practice/app.js
@@ -27,13 +27,61 @@
 //     product.value = "";
 // })
 
-const h1 = document.querySelector("h1");
-const input = document.querySelector("input");
+// const h1 = document.querySelector("h1");
+// const input = document.querySelector("input");
  
-input.addEventListener("input", function() {
-    if(this.value) {
-        h1.innerText = `Welcome, ${this.value}`;
-    } else {
-        h1.innerText = "Enter Your Username";
-    }
-});
+// input.addEventListener("input", function() {
+//     if(this.value) {
+//         h1.innerText = `Welcome, ${this.value}`;
+//     } else {
+//         h1.innerText = "Enter Your Username";
+//     }
+// });
+
+// function rollDie() {
+//     let roll = Math.floor(Math.random()* 6 + 1);
+//     console.log(`Rolled: ${roll}`)
+// }
+// rollDie();
+
+// function greet(firstName) {
+//     console.log(firstName);
+// }
+// greet('Sean')
+
+// function rollDie() {
+//         let roll = Math.floor(Math.random()* 6) + 1;
+//         console.log(`Rolled: ${roll}`);
+// }
+// function throwDice(numRolls) {
+//     for(i = 0; i < numRolls; i++) {
+//     rollDie()  
+//     }
+// }
+// throwDice(1);
+
+function rollDie() {
+    let die1 = Math.floor(Math.random()* 6) + 1;
+    let die2 = Math.floor(Math.random()* 6) + 1;
+    let die3 = Math.floor(Math.random()* 6) + 1;
+    let die4 = Math.floor(Math.random()* 6) + 1;
+    let die5 = Math.floor(Math.random()* 6) + 1;
+    console.log(`Die 1: ${die1}`);
+    console.log(`Die 2: ${die2}`);
+    console.log(`Die 3: ${die3}`);
+    console.log(`Die 4: ${die4}`);
+    console.log(`Die 5: ${die5}`);
+}
+function throwDice(numRolls) {
+for(i = 0; i < numRolls; i++) {
+rollDie()  
+}
+}
+throwDice(1);
+
+
+
+
+
+
+             
